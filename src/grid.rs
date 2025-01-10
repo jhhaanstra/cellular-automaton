34d04c2938b05ef864c5406ad5212a4c6ce13541
jhhaanstra <janhendrikhaanstra@gmail.com>
2025-01-10 21:47:48 +0100

    Implement our own version of Vector

--- src/grid.rs
+++ src/grid.rs
@@ -1,68 +1,128 @@
-use std::collections::HashMap;
-use vector2d::Vector2D;
+use std::collections::HashSet;
+use std::ops::Add;
 
 pub struct Grid {
-    cells: HashMap<(i32, i32), bool>
+    cells: HashSet<Vector>
 }
 
 impl Grid {
-    const DIRECTIONS: [Vector2D<i32>; 8] = [
-        Vector2D{y: -1, x: -1},
-        Vector2D{y: -1, x: 0},
-        Vector2D{y: -1, x: 1},
-        Vector2D{y: 0,  x: -1},
-        Vector2D{y: 0,  x: 1},
-        Vector2D{y: 1,  x: -1},
-        Vector2D{y: 1,  x: 0},
-        Vector2D{y: 1,  x: 1},
+    const DIRECTIONS: [Vector; 8] = [
+        Vector{y: -1, x: -1},
+        Vector{y: -1, x: 0},
+        Vector{y: -1, x: 1},
+        Vector{y: 0,  x: -1},
+        Vector{y: 0,  x: 1},
+        Vector{y: 1,  x: -1},
+        Vector{y: 1,  x: 0},
+        Vector{y: 1,  x: 1},
     ];
 
     pub fn new() -> Grid {
-        Grid{cells: HashMap::new()}
+        Grid{cells: HashSet::new()}
     }
 
-    pub fn add_cells(&mut self, cells: &Vec<&Vector2D<i32>>) {
+    pub fn add_cells(&mut self, cells: &Vec<Vector>) {
         cells.iter().for_each(|cell| self.add_cell(cell));
     }
 
-    pub fn add_cell(&mut self, cell: &Vector2D<i32>) {
-        self.place(cell, true)
+    pub fn add_cell(&mut self, cell: &Vector) {
+        self.cells.insert(cell.clone());
     }
 
-    pub fn remove_cell(&mut self, cell: &Vector2D<i32>) {
-        self.cells.remove(&Self::to_tuple(cell));
+    pub fn remove_cell(&mut self, cell: &Vector) {
+        self.cells.remove(cell);
     }
 
-    fn place(&mut self, cell: &Vector2D<i32>, value: bool) {
-        self.cells.insert(Self::to_tuple(cell), value);
+    pub fn contains(&self, cell: &Vector) -> bool {
+        self.cells.contains(cell)
     }
 
-    pub fn contains(&self, cell: &Vector2D<i32>) -> bool {
-        self.cells.get(&Self::to_tuple(cell)).unwrap_or(&false).clone()
-    }
-
-    pub fn get_neighbours(&self, cell: &Vector2D<i32>) -> Vec<Vector2D<i32>> {
+    pub fn get_neighbours(&self, cell: &Vector) -> Vec<Vector> {
         Self::DIRECTIONS.iter()
-            .map(|direction| cell + &direction)
-            .filter(|point| *self.cells.get(&Self::to_tuple(&point))
-                .unwrap_or(&false))
+            .map(|direction| cell + direction)
+            .filter(|point| self.cells.contains(point))
             .collect()
     }
+}
 
-    fn to_tuple(point: &Vector2D<i32>) -> (i32, i32) {
-        (point.x, point.y)
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
+pub struct Vector {
+    x: i32,
+    y: i32
+}
+
+impl Vector {
+
+    pub fn new(x: i32, y: i32) -> Vector {
+        Vector{x, y}
+    }
+
+}
+
+impl Add for Vector {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self::Output {
+        Vector::new(self.x + other.x, self.y + other.y)
+    }
+}
+
+impl Add<&Vector> for Vector {
+    type Output = Vector;
+
+    fn add(self, other: &Vector) -> Vector {
+        Vector {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
+impl Add<&Vector> for &Vector {
+    type Output = Vector;
+
+    fn add(self, other: &Vector) -> Vector {
+        Vector {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
     }
 }
 
 #[cfg(test)]
-mod tests {
-    use vector2d::Vector2D;
-    use crate::grid::Grid;
+mod vector_tests {
+    use crate::grid::Vector;
+
+    #[test]
+    fn should_add() {
+        let a = Vector::new(1, 2);
+        let b = Vector::new(3, 4);
+        assert_eq!(Vector::new(4, 6), a + b);
+    }
+
+    #[test]
+    fn should_add_with_reference() {
+        let a = Vector::new(1, 2);
+        let b = &Vector::new(3, 4);
+        assert_eq!(Vector::new(4, 6), a + b);
+    }
+
+    #[test]
+    fn should_add_with_both_references() {
+        let a = &Vector::new(1, 2);
+        let b = &Vector::new(3, 4);
+        assert_eq!(Vector::new(4, 6), a + b);
+    }
+}
+
+#[cfg(test)]
+mod grid_tests {
+    use crate::grid::{Grid, Vector};
 
     #[test]
     fn should_add_cell() {
         let mut grid = Grid::new();
-        let point = &Vector2D { x: 1, y: 2 };
+        let point = &Vector { x: 1, y: 2 };
         grid.add_cell(point);
 
         assert!(grid.contains(point));
@@ -71,7 +131,7 @@ mod tests {
     #[test]
     fn should_remove_cell() {
         let mut grid = Grid::new();
-        let point = &Vector2D { x: 1, y: 2 };
+        let point = &Vector { x: 1, y: 2 };
         grid.add_cell(point);
         grid.remove_cell(point);
 
@@ -80,8 +140,8 @@ mod tests {
 
     #[test]
     fn should_find_no_neighbours() {
-        let mut grid = Grid::new();
-        let point = &Vector2D { x: 1, y: 1 };
+        let grid = Grid::new();
+        let point = &Vector { x: 1, y: 1 };
         assert_eq!(grid.get_neighbours(point), vec![]);
     }
 
@@ -89,25 +149,25 @@ mod tests {
     fn should_find_neighbours() {
         let mut grid = Grid::new();
         grid.add_cells(&vec![
-            &Vector2D::new(0, 2),
-            &Vector2D::new(1, 2),
-            &Vector2D::new(2, 2),
-            &Vector2D::new(0, 1),
-            &Vector2D::new(2, 1),
-            &Vector2D::new(0, 0),
-            &Vector2D::new(1, 0),
-            &Vector2D::new(2, 0),
+            Vector::new(0, 2),
+            Vector::new(1, 2),
+            Vector::new(2, 2),
+            Vector::new(0, 1),
+            Vector::new(2, 1),
+            Vector::new(0, 0),
+            Vector::new(1, 0),
+            Vector::new(2, 0),
         ]);
-        let point = &Vector2D { x: 1, y: 1 };
-        let neighbours = grid.get_neighbours(point);
+        let point = Vector { x: 1, y: 1 };
+        let neighbours = grid.get_neighbours(&point);
         assert_eq!(neighbours.len(), 8);
-        assert!(neighbours.contains(&Vector2D::new(0, 2)));
-        assert!(neighbours.contains(&Vector2D::new(1, 2)));
-        assert!(neighbours.contains(&Vector2D::new(2, 2)));
-        assert!(neighbours.contains(&Vector2D::new(0, 1)));
-        assert!(neighbours.contains(&Vector2D::new(2, 1)));
-        assert!(neighbours.contains(&Vector2D::new(0, 0)));
-        assert!(neighbours.contains(&Vector2D::new(1, 0)));
-        assert!(neighbours.contains(&Vector2D::new(2, 0)));
+        assert!(neighbours.contains(&Vector::new(0, 2)));
+        assert!(neighbours.contains(&Vector::new(1, 2)));
+        assert!(neighbours.contains(&Vector::new(2, 2)));
+        assert!(neighbours.contains(&Vector::new(0, 1)));
+        assert!(neighbours.contains(&Vector::new(2, 1)));
+        assert!(neighbours.contains(&Vector::new(0, 0)));
+        assert!(neighbours.contains(&Vector::new(1, 0)));
+        assert!(neighbours.contains(&Vector::new(2, 0)));
     }
 }
--- src/rules.rs
+++ src/rules.rs
@@ -1,117 +1,114 @@
-use crate::grid::Grid;
-use vector2d::Vector2D;
+use crate::grid::{Grid, Vector};
 
 pub trait Rule {
-    fn complies(point: Vector2D<i32>, grid: Grid) -> bool;
+    fn complies(point: Vector, grid: Grid) -> bool;
 }
 
 struct HasTwoOrThreeNeighbours;
 struct HasThreeNeighbours;
 impl Rule for HasTwoOrThreeNeighbours {
-    fn complies(point: Vector2D<i32>, grid: Grid) -> bool {
+    fn complies(point: Vector, grid: Grid) -> bool {
         let nr_neighbours = grid.get_neighbours(&point).len();
         nr_neighbours == 2 || nr_neighbours == 3
     }
 }
 
 impl Rule for HasThreeNeighbours {
-    fn complies(point: Vector2D<i32>, grid: Grid) -> bool {
+    fn complies(point: Vector, grid: Grid) -> bool {
         grid.get_neighbours(&point).len() == 3
     }
 }
 
 #[cfg(test)]
 mod has_two_or_three_neighbours_tests {
-    use crate::grid::Grid;
+    use crate::grid::{Grid, Vector};
     use crate::rules::{HasTwoOrThreeNeighbours, Rule};
-    use vector2d::Vector2D;
 
     #[test]
     fn when_two_neighbours_then_complies() {
         let mut grid = Grid::new();
         grid.add_cells(&vec![
-            &Vector2D::new(2, 2),
-            &Vector2D::new(0, 0),
+            Vector::new(2, 2),
+            Vector::new(0, 0),
         ]);
 
-        assert!(HasTwoOrThreeNeighbours::complies(Vector2D::new(1, 1), grid));
+        assert!(HasTwoOrThreeNeighbours::complies(Vector::new(1, 1), grid));
     }
 
     #[test]
     fn when_three_neighbours_then_complies() {
         let mut grid = Grid::new();
         grid.add_cells(&vec![
-            &Vector2D::new(2, 2),
-            &Vector2D::new(0, 0),
-            &Vector2D::new(2, 1),
+            Vector::new(2, 2),
+            Vector::new(0, 0),
+            Vector::new(2, 1),
         ]);
 
-        assert!(HasTwoOrThreeNeighbours::complies(Vector2D::new(1, 1), grid));
+        assert!(HasTwoOrThreeNeighbours::complies(Vector::new(1, 1), grid));
     }
 
     #[test]
     fn when_less_than_two_neighbours_then_does_not_comply() {
         let mut grid = Grid::new();
         grid.add_cells(&vec![
-            &Vector2D::new(2, 2),
+            Vector::new(2, 2),
         ]);
 
-        assert!(!HasTwoOrThreeNeighbours::complies(Vector2D::new(1, 1), grid));
+        assert!(!HasTwoOrThreeNeighbours::complies(Vector::new(1, 1), grid));
     }
 
     #[test]
     fn when_more_than_three_neighbours_then_does_not_comply() {
         let mut grid = Grid::new();
         grid.add_cells(&vec![
-            &Vector2D::new(2, 2),
-            &Vector2D::new(0, 0),
-            &Vector2D::new(2, 1),
-            &Vector2D::new(1, 2),
+            Vector::new(2, 2),
+            Vector::new(0, 0),
+            Vector::new(2, 1),
+            Vector::new(1, 2),
         ]);
 
-        assert!(!HasTwoOrThreeNeighbours::complies(Vector2D::new(1, 1), grid));
+        assert!(!HasTwoOrThreeNeighbours::complies(Vector::new(1, 1), grid));
     }
 }
 
 #[cfg(test)]
 mod has_three_neighbours_tests {
-    use crate::grid::Grid;
+    use crate::grid::{Grid, Vector};
     use crate::rules::{HasThreeNeighbours, Rule};
-    use vector2d::Vector2D;
 
     #[test]
     fn when_three_neighbours_then_complies() {
         let mut grid = Grid::new();
         grid.add_cells(&vec![
-            &Vector2D::new(2, 2),
-            &Vector2D::new(0, 0),
-            &Vector2D::new(2, 1),
+            Vector::new(2, 2),
+            Vector::new(0, 0),
+            Vector::new(2, 1),
         ]);
 
-        assert!(HasThreeNeighbours::complies(Vector2D::new(1, 1), grid));
+        assert!(HasThreeNeighbours::complies(Vector::new(1, 1), grid));
     }
 
     #[test]
     fn when_less_than_three_neighbours_then_does_not_comply() {
         let mut grid = Grid::new();
         grid.add_cells(&vec![
-            &Vector2D::new(2, 1),
-            &Vector2D::new(1, 2),
+            Vector::new(2, 1),
+            Vector::new(1, 2),
         ]);
 
-        assert!(!HasThreeNeighbours::complies(Vector2D::new(1, 1), grid));
+        assert!(!HasThreeNeighbours::complies(Vector::new(1, 1), grid));
     }
 
     #[test]
     fn when_more_than_three_neighbours_then_does_not_comply() {
         let mut grid = Grid::new();
         grid.add_cells(&vec![
-            &Vector2D::new(2, 2),
-            &Vector2D::new(0, 0),
-            &Vector2D::new(2, 1),
-            &Vector2D::new(1, 2),
+            Vector::new(2, 2),
+            Vector::new(0, 0),
+            Vector::new(2, 1),
+            Vector::new(1, 2),
         ]);
 
-        assert!(!HasThreeNeighbours::complies(Vector2D::new(1, 1), grid));
+        assert!(!HasThreeNeighbours::complies(Vector::new(1, 1), grid));
     }
 }
