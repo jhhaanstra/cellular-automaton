6c66ab2ae7b28c8d7db2dfee91f2ebedb5002891
jhhaanstra <janhendrikhaanstra@gmail.com>
2025-01-12 23:01:24 +0100

    Implemented a way to display the simulation in a grid. Also added a command line setup but that's not used just yet.

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,62 @@ use crate::grid::{Grid, Vector};
 use crate::rules::{HasThreeNeighbours, HasTwoOrThreeNeighbours, Rule};
 use std::collections::HashSet;
 use std::fmt::{Display, Formatter};
+use std::thread;
+use std::time::Duration;
+use clap::{Arg, ArgAction, Command};
+use clap::builder::TypedValueParser;
+use regex::Regex;
+
+// For later
+fn cli() -> Command {
+    Command::new("git")
+        .about("A fictional versioning CLI")
+        .version("0.1")
+        .arg(
+            Arg::new("input")
+                .short('i')
+                .long("input")
+                .help("List of cells that should be alive when the simulation starts. Concatenated by a semicolon. E.g. 1,2;3,4 maps to the cells x1,y2 and x3,y4 to be alive when the simulation starts.")
+                // .required(true)
+                .default_value("1,1;1,2;1,3")
+                .action(ArgAction::Set)
+        )
+        .arg(
+            Arg::new("dimensions")
+                .short('d') // Short flag, e.g., -n
+                .long("dimensions") // Long flag, e.g., --name
+                .value_name("DIMENSIONS")
+                .help("Dimensions of the grid in the display (e.g. 10x10 when 10 is provided)")
+                .action(ArgAction::Set)
+        )
+}
 
 fn main() {
-    println!("Hello, world!");
+    let grid = Grid {
+        cells: parse_input(&String::from("20,19;19,20;20,20;20,21;21,21"))
+    };
+
+    let mut game = Game::new(40, grid);
+    let running: bool = true;
+
+    while running {
+        std::process::Command::new("clear").status().unwrap();
+        print!("{}", game);
+        thread::sleep(Duration::from_millis(50));
+        game.update();
+    }
+}
+
+fn parse_input(input: &String) -> HashSet<Vector> {
+    let vector_regex = Regex::new("(?P<x>\\d+),(?P<y>\\d+)").unwrap();
+    input.split(";")
+        .into_iter()
+        .map(|vec| vector_regex.captures(vec).unwrap())
+        .map(|cap| Vector::new(
+            cap["x"].parse().expect("Invalid input provided, expected format is x,y;x,y..."),
+            cap["y"].parse().expect("Invalid input provided, expected format is x,y;x,y..."))
+        )
+        .collect()
 }
 
 struct Game {
@@ -70,8 +123,16 @@ impl Display for Game {
 
 #[cfg(test)]
 mod tests {
+    use std::collections::HashSet;
     use crate::grid::{Grid, Vector};
-    use crate::Game;
+    use crate::{parse_input, Game};
+
+    #[test]
+    fn should_parse_input() {
+        let actual = parse_input(&String::from("1,2;3,4;7,45"));
+        let expected: HashSet<Vector> = vec!(Vector::new(1, 2), Vector::new(3, 4), Vector::new(7, 45)).into_iter().collect();
+        assert_eq!(actual, expected);
+    }
 
     #[test]
     fn display_game_state() {
